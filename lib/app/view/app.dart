4cb1689ba27a7dcfad9551867b9762052371fc4c
LuckUVeryX <luckuveryx@icloud.com>
2023-05-27 11:56:09 +0800

    theme: add configs

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,28 +1,19 @@
 import 'package:evlve/app/theme/theme.dart';
 import 'package:evlve/l10n/l10n.dart';
+import 'package:evlve/modules/auth/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = GoogleFonts.notoSansMonoTextTheme();
-
     return MaterialApp(
-      theme: ThemeData.from(
-        useMaterial3: true,
-        textTheme: textTheme,
-        colorScheme: Palette.light,
-      ),
-      darkTheme: ThemeData.from(
-        useMaterial3: true,
-        textTheme: textTheme,
-        colorScheme: Palette.dark,
-      ),
+      theme: ThemeConfig.light,
+      darkTheme: ThemeConfig.dark,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      home: const SigninPage(),
     );
   }
 }
