966b4ed7a531f07173e875aa9262989f9ced15e6
LuckUVeryX <luckuveryx@icloud.com>
2023-05-27 16:02:19 +0800

    fix: broken test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,12 @@
 import 'package:evlve/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+    testWidgets('renders MaterialApp', (tester) async {
+      await tester.pumpWidget(const ProviderScope(child: App()));
       expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
