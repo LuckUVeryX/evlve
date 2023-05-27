145ca3da06211e3b6781b613ff264c1e8c26a270
LuckUVeryX <luckuveryx@icloud.com>
2023-05-27 22:11:03 +0800

    refactor: remove unnecessary buildcontext

--- lib/utils/ref_extensions.dart
+++ lib/utils/ref_extensions.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 extension WidgetRefX on WidgetRef {
-  void listenErrors(
-    BuildContext context,
-    List<ProviderListenable<AsyncValue<dynamic>>> providers,
-  ) {
+  void listenErrors(List<ProviderListenable<AsyncValue<dynamic>>> providers) {
     for (final provider in providers) {
       listen(
         provider,
