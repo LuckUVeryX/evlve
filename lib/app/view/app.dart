c88c22ed2ec1240434eb6bbaaf7298eb9964567c
LuckUVeryX <luckuveryx@icloud.com>
2023-05-27 11:33:26 +0800

    feat: add color scheme and textTheme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,17 +1,25 @@
+import 'package:evlve/app/theme/theme.dart';
 import 'package:evlve/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = GoogleFonts.notoSansMonoTextTheme();
+
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
+      theme: ThemeData.from(
+        useMaterial3: true,
+        textTheme: textTheme,
+        colorScheme: Palette.light,
+      ),
+      darkTheme: ThemeData.from(
+        useMaterial3: true,
+        textTheme: textTheme,
+        colorScheme: Palette.dark,
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
