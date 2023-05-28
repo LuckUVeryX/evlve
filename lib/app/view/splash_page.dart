3430da1b65e4165f22f5e871d07ecb670e1d4358
LuckUVeryX <luckuveryx@icloud.com>
2023-05-28 12:00:10 +0800

    feat: listen to stay logged in errors in splash page

--- lib/app/view/splash_page.dart
+++ lib/app/view/splash_page.dart
@@ -1,10 +1,14 @@
+import 'package:evlve/modules/auth/auth.dart';
+import 'package:evlve/utils/ref_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends ConsumerWidget {
   const SplashPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listenErrors([authControllerProvider]);
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
