dfaf02d3502ed00445668041286c6bd688baa67c
LuckUVeryX <luckuveryx@icloud.com>
2023-05-29 11:52:55 +0800

    UI: enhance error UI state

--- lib/modules/schedule/views/widgets/schedule_list_view.dart
+++ lib/modules/schedule/views/widgets/schedule_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:evlve/app/theme/theme_extensions.dart';
 import 'package:evlve/app/views/shimmer_widget.dart';
 import 'package:evlve/modules/booking/booking.dart';
 import 'package:evlve/modules/schedule/schedule.dart';
@@ -23,83 +24,101 @@ class ScheduleListView extends ConsumerWidget {
     ref.listenErrors([provider]);
 
     final scheduleValue = ref.watch(provider);
-    return RefreshIndicator(
-      onRefresh: () async => ref.refresh(provider.future),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return scheduleValue.when(
-            data: (scheduleList) {
-              final schedules = scheduleList.schedules;
-              if (index >= schedules.length) return null;
-              final schedule = scheduleList.schedules[index];
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return scheduleValue.when(
+          data: (scheduleList) {
+            final schedules = scheduleList.schedules;
+            if (index >= schedules.length) return null;
+            final schedule = scheduleList.schedules[index];
 
-              final bookingProvider =
-                  bookingControllerProvider(schedule: schedule);
-              final booking = ref.watch(bookingProvider);
+            final bookingProvider =
+                bookingControllerProvider(schedule: schedule);
+            final booking = ref.watch(bookingProvider);
 
-              final details = booking.value?.schedule.event.classDetails ??
-                  schedule.event.classDetails;
+            final details = booking.value?.schedule.event.classDetails ??
+                schedule.event.classDetails;
 
-              final canBook = details.canBook && !booking.isLoading;
-              final value = details.isCP ? null : details.isBookedByMe;
+            final canBook = details.canBook && !booking.isLoading;
+            final value = details.isCP ? null : details.isBookedByMe;
 
-              return Consumer(
-                builder: (context, ref, child) {
-                  ref
-                    ..listenErrors([bookingProvider])
-                    ..listen(
-                      bookingProvider,
-                      (prev, next) {
-                        if (next.hasError) return;
-                        final msg = next.value?.message;
-                        if (msg == null) return;
-                        if (prev?.value?.message == msg) return;
-                        ScaffoldMessenger.of(context)
-                          ..hideCurrentSnackBar()
-                          ..showSnackBar(SnackBar(content: Text(msg)));
-                      },
-                    );
-
-                  return CheckboxListTile(
-                    dense: true,
-                    visualDensity: VisualDensity.compact,
-                    tristate: true,
-                    value: value,
-                    onChanged: !canBook
-                        ? null
-                        : (_) async {
-                            if (schedule.isLateBooking) {
-                              final confirm =
-                                  await LateBookingConfirmDialog.show(context);
-                              if (confirm != true) return;
-                            }
-
-                            final notifier = ref.read(bookingProvider.notifier);
-                            details.isBookedByMe
-                                ? await notifier.cancel()
-                                : await notifier.book();
-                          },
-                    title: Text(
-                      schedule.title,
-                    ),
-                    subtitle: Text(
-                      '''
-${DateFormat.Hm().format(schedule.start)}|${schedule.end.difference(schedule.start).inMinutes}m''',
-                    ),
+            return Consumer(
+              builder: (context, ref, child) {
+                ref
+                  ..listenErrors([bookingProvider])
+                  ..listen(
+                    bookingProvider,
+                    (prev, next) {
+                      if (next.hasError) return;
+                      final msg = next.value?.message;
+                      if (msg == null) return;
+                      if (prev?.value?.message == msg) return;
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(SnackBar(content: Text(msg)));
+                    },
                   );
-                },
-              );
-            },
-            error: (e, st) {
-              return index == 0 ? Text(e.toString()) : null;
-            },
-            loading: () {
-              return const _ScheduleLoadingShimmer();
-            },
-          );
-        },
-      ),
+                return CheckboxListTile(
+                  dense: true,
+                  visualDensity: VisualDensity.compact,
+                  tristate: true,
+                  value: value,
+                  onChanged: !canBook
+                      ? null
+                      : (_) async {
+                          if (schedule.isLateBooking) {
+                            final confirm =
+                                await LateBookingConfirmDialog.show(context);
+                            if (confirm != true) return;
+                          }
+
+                          final notifier = ref.read(bookingProvider.notifier);
+                          details.isBookedByMe
+                              ? await notifier.cancel()
+                              : await notifier.book();
+                        },
+                  title: Text(
+                    schedule.title,
+                  ),
+                  subtitle: Text(
+                    '''
+${DateFormat.Hm().format(schedule.start)}|${schedule.end.difference(schedule.start).inMinutes}m''',
+                  ),
+                );
+              },
+            );
+          },
+          loading: () => const _ScheduleLoadingShimmer(),
+          error: (e, st) {
+            if (index != 0) return null;
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton.icon(
+                    onPressed: () {
+                      ref
+                        ..invalidate(provider)
+                        ..invalidate(bookingControllerProvider);
+                    },
+                    icon: const Icon(Icons.refresh),
+                    label: const Text('Retry'),
+                  ),
+                  Text(
+                    e.toString(),
+                    style: context.textTheme.bodyMedium?.copyWith(
+                      color: context.colorScheme.error,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
