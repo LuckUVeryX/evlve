d83a5971e393289a64fc5cd2e5878a7eed34dc22
LuckUVeryX <luckuveryx@icloud.com>
2023-05-29 15:03:44 +0800

    UI: update schedule appbar logic

--- lib/modules/schedule/views/widgets/schedule_app_bar.dart
+++ lib/modules/schedule/views/widgets/schedule_app_bar.dart
@@ -14,67 +14,84 @@ class ScheduleAppBar extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final facility = ref.watch(scheduleFacilityControllerProvider);
-    final areas = facility.areas;
 
     return SliverAppBar(
       pinned: true,
       floating: true,
-      snap: true,
       titleSpacing: 0,
-      toolbarHeight: kToolbarHeight + 92,
-      bottom: TabBar(tabs: [for (final area in areas) Tab(text: area.key.key)]),
-      title: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [_ScheduleLocation(), ScheduleDayPicker()],
-      ),
+      bottom: const _AppBarBottom(),
+      leading: const _LocationIconButton(),
+      title: Text(facility.key.key),
+      centerTitle: false,
+      actions: const [_QRIconButton()],
     );
   }
 }
 
-class _ScheduleLocation extends ConsumerWidget {
-  const _ScheduleLocation();
+class _AppBarBottom extends ConsumerWidget implements PreferredSizeWidget {
+  const _AppBarBottom();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final facility = ref.watch(scheduleFacilityControllerProvider);
+    final areas = facility.areas;
+    return Column(
+      children: [
+        const ScheduleDayPicker(),
+        TabBar(tabs: [for (final area in areas) Tab(text: area.key.key)]),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(92 + kToolbarHeight);
+}
+
+class _QRIconButton extends ConsumerWidget {
+  const _QRIconButton();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
-    final facility = ref.watch(scheduleFacilityControllerProvider);
+    return IconButton(
+      onPressed: () async {
+        await QRDialog.show(context, id: user.id);
+        if (context.mounted) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Text(context.l10n.qrDialogShakeSnackbar),
+              ),
+            );
+        }
+      },
+      icon: const Icon(Icons.qr_code_rounded),
+    );
+  }
+}
 
-    return Row(
-      children: [
-        IconButton(
-          onPressed: !user.isBlackCard
-              ? null
-              : () async {
-                  final newFacility =
-                      await ScheduleFacilityDialog.show(context);
-                  if (newFacility == null) return;
-                  ref
-                      .read(scheduleFacilityControllerProvider.notifier)
-                      .onFacilityChanged(newFacility);
-                },
-          icon: Icon(
-            Icons.place_outlined,
-            color: context.colorScheme.onBackground,
-          ),
-        ),
-        Text(facility.key.key),
-        const Spacer(),
-        IconButton(
-          onPressed: () async {
-            await QRDialog.show(context, id: user.id);
-            if (context.mounted) {
-              ScaffoldMessenger.of(context)
-                ..hideCurrentSnackBar()
-                ..showSnackBar(
-                  SnackBar(
-                    content: Text(context.l10n.qrDialogShakeSnackbar),
-                  ),
-                );
-            }
-          },
-          icon: const Icon(Icons.qr_code_rounded),
-        ),
-      ],
+class _LocationIconButton extends ConsumerWidget {
+  const _LocationIconButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider);
+
+    return IconButton(
+      onPressed: !user.isBlackCard
+          ? null
+          : () async {
+              final newFacility = await ScheduleFacilityDialog.show(context);
+              if (newFacility == null) return;
+              ref
+                  .read(scheduleFacilityControllerProvider.notifier)
+                  .onFacilityChanged(newFacility);
+            },
+      icon: Icon(
+        Icons.place_outlined,
+        color: context.colorScheme.onBackground,
+      ),
     );
   }
 }
