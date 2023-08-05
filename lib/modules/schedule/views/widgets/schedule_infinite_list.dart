b71a5f38ed065c6233f7d44a61af87124996bbb6
LuckUVeryX <luckuveryx@icloud.com>
2023-08-05 16:24:02 +0800

    Fix: missing error handling in infinite list

--- lib/modules/schedule/views/widgets/schedule_infinite_list.dart
+++ lib/modules/schedule/views/widgets/schedule_infinite_list.dart
@@ -4,6 +4,7 @@ import 'package:evlve/modules/facility/facility.dart';
 import 'package:evlve/modules/schedule/schedule.dart';
 import 'package:evlve/theme/utils/utils.dart';
 import 'package:evlve/utils/date_utils.dart';
+import 'package:evlve/utils/ref_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -63,7 +64,7 @@ class _ScheduleInfiniteListState extends ConsumerState<ScheduleInfiniteList> {
 
 const _pageSize = 48;
 
-class _ReverseList extends ConsumerWidget {
+class _ReverseList extends StatelessWidget {
   const _ReverseList({
     required this.area,
   });
@@ -71,67 +72,78 @@ class _ReverseList extends ConsumerWidget {
   final Area area;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return SliverList(
-      delegate: SliverChildBuilderDelegate((context, index) {
-        final page = index ~/ _pageSize + 1;
-        final indexInPage = index % _pageSize;
-        final date = DateTime.now().subtract(Duration(days: page)).stripTime();
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final page = index ~/ _pageSize + 1;
+          final indexInPage = index % _pageSize;
+          final date =
+              DateTime.now().subtract(Duration(days: page)).stripTime();
 
-        final provider = scheduleProvider(date: date, area: area);
+          final provider = scheduleProvider(date: date, area: area);
 
-        final scheduleValue = ref.watch(provider);
-        return scheduleValue.when(
-          data: (scheduleList) {
-            final schedules = scheduleList.schedules.reversed.toList();
-            if (indexInPage >= schedules.length) return const Offstage();
+          return Consumer(
+            builder: (context, ref, child) {
+              final scheduleValue = ref.watch(provider);
+              ref.listenErrors([provider]);
+              return scheduleValue.when(
+                data: (scheduleList) {
+                  final schedules = scheduleList.schedules.reversed.toList();
+                  if (indexInPage >= schedules.length) return const Offstage();
 
-            final schedule = schedules[indexInPage];
+                  final schedule = schedules[indexInPage];
 
-            return Column(
-              children: [
-                if (indexInPage == schedules.length - 1)
-                  ScheduleDateHeader(dt: date),
-                ScheduleListItem(schedule: schedule),
-              ],
-            );
-          },
-          error: (e, st) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      ref
-                        ..invalidate(provider)
-                        ..invalidate(bookingControllerProvider);
-                    },
-                    icon: const Icon(Icons.refresh),
-                    label: Text(context.l10n.scheduleListViewErrorRetryButton),
-                  ),
-                  Text(
-                    e.toString(),
-                    style: context.textTheme.bodyMedium?.copyWith(
-                      color: context.colorScheme.error,
+                  return Column(
+                    children: [
+                      if (indexInPage == schedules.length - 1)
+                        ScheduleDateHeader(dt: date),
+                      ScheduleListItem(schedule: schedule),
+                    ],
+                  );
+                },
+                error: (e, st) {
+                  if (indexInPage != 0) return const Offstage();
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 16,
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
-          loading: () => const ScheduleListItemShimmer(),
-        );
-      }),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {
+                            ref
+                              ..invalidate(provider)
+                              ..invalidate(bookingControllerProvider);
+                          },
+                          icon: const Icon(Icons.refresh),
+                          label: Text(
+                            context.l10n.scheduleListViewErrorRetryButton,
+                          ),
+                        ),
+                        Text(
+                          e.toString(),
+                          style: context.textTheme.bodyMedium?.copyWith(
+                            color: context.colorScheme.error,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                loading: () => const ScheduleListItemShimmer(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class _ForwardList extends ConsumerWidget {
+class _ForwardList extends StatelessWidget {
   const _ForwardList({
     required this.area,
     super.key,
@@ -140,36 +152,71 @@ class _ForwardList extends ConsumerWidget {
   final Area area;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return SliverList(
-      delegate: SliverChildBuilderDelegate((context, index) {
-        final page = index ~/ _pageSize;
-        final indexInPage = index % _pageSize;
-        final date = DateTime.now().add(Duration(days: page)).stripTime();
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final page = index ~/ _pageSize;
+          final indexInPage = index % _pageSize;
+          final date = DateTime.now().add(Duration(days: page)).stripTime();
 
-        final provider = scheduleProvider(date: date, area: area);
+          final provider = scheduleProvider(date: date, area: area);
 
-        final scheduleValue = ref.watch(provider);
-        return scheduleValue.when(
-          data: (scheduleList) {
-            final schedules = scheduleList.schedules;
+          return Consumer(
+            builder: (context, ref, child) {
+              ref.listenErrors([provider]);
+              final scheduleValue = ref.watch(provider);
+              return scheduleValue.when(
+                data: (scheduleList) {
+                  final schedules = scheduleList.schedules;
 
-            if (indexInPage >= schedules.length) return const Offstage();
+                  if (indexInPage >= schedules.length) return const Offstage();
 
-            final schedule = schedules[indexInPage];
-            return Column(
-              children: [
-                if (indexInPage == 0) ScheduleDateHeader(dt: date),
-                ScheduleListItem(schedule: schedule),
-              ],
-            );
-          },
-          error: (error, stackTrace) {
-            return Text(error.toString());
-          },
-          loading: () => const ScheduleListItemShimmer(),
-        );
-      }),
+                  final schedule = schedules[indexInPage];
+                  return Column(
+                    children: [
+                      if (indexInPage == 0) ScheduleDateHeader(dt: date),
+                      ScheduleListItem(schedule: schedule),
+                    ],
+                  );
+                },
+                error: (e, st) {
+                  if (indexInPage != 0) return const Offstage();
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 16,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {
+                            ref
+                              ..invalidate(provider)
+                              ..invalidate(bookingControllerProvider);
+                          },
+                          icon: const Icon(Icons.refresh),
+                          label: Text(
+                            context.l10n.scheduleListViewErrorRetryButton,
+                          ),
+                        ),
+                        Text(
+                          e.toString(),
+                          style: context.textTheme.bodyMedium?.copyWith(
+                            color: context.colorScheme.error,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                loading: () => const ScheduleListItemShimmer(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
