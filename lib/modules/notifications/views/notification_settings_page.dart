5c2f026160c7128a47ae78f340f01eb6042fc9c8
LuckUVeryX <luckuveryx@icloud.com>
2023-08-06 17:04:21 +0800

    fix: notifications duration picker not working

--- lib/modules/notifications/views/notification_settings_page.dart
+++ lib/modules/notifications/views/notification_settings_page.dart
@@ -18,24 +18,20 @@ class NotificationSettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar.large(
-            title: Text(context.l10n.settingsNotifications),
-            actions: const [if (kDebugMode) _DebugNotificationIconButton()],
+      appBar: AppBar(
+        title: Text(context.l10n.settingsNotifications),
+        actions: const [if (kDebugMode) _DebugNotificationIconButton()],
+      ),
+      body: const Column(
+        children: [
+          _UpcomingClassesSwitchListTile(),
+          SizedBox(height: 16),
+          _DurationPicker(),
+          SizedBox(height: 24),
+          Expanded(
+            child: _UpcomingNotificationsListView(),
           ),
         ],
-        body: const Column(
-          children: [
-            _UpcomingClassesSwitchListTile(),
-            SizedBox(height: 16),
-            _DurationPicker(),
-            SizedBox(height: 24),
-            Expanded(
-              child: _UpcomingNotificationsListView(),
-            ),
-          ],
-        ),
       ),
     );
   }
