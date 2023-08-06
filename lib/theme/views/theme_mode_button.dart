bd2764aa21b6f90eec133e95628a6151be094264
LuckUVeryX <luckuveryx@icloud.com>
2023-08-06 15:38:31 +0800

    feat: theme selector neu design

--- lib/theme/views/theme_mode_button.dart
+++ lib/theme/views/theme_mode_button.dart
@@ -1,3 +1,4 @@
+import 'package:evlve/app/app.dart';
 import 'package:evlve/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,24 +10,32 @@ class ThemeModeButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeControllerProvider);
 
-    return SegmentedButton<ThemeMode>(
-      selected: {themeMode},
-      segments: const [
-        ButtonSegment(
-          value: ThemeMode.light,
-          icon: Icon(Icons.light_mode_outlined),
+    return NeuContainer(
+      color: context.colorScheme.background,
+      child: SegmentedButton<ThemeMode>(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.resolveWith((_) {
+            return const RoundedRectangleBorder();
+          }),
         ),
-        ButtonSegment(
-          value: ThemeMode.system,
-          icon: Icon(Icons.settings_suggest_outlined),
-        ),
-        ButtonSegment(
-          value: ThemeMode.dark,
-          icon: Icon(Icons.dark_mode_outlined),
-        ),
-      ],
-      onSelectionChanged:
-          ref.read(themeControllerProvider.notifier).onThemeModeChanged,
+        selected: {themeMode},
+        segments: const [
+          ButtonSegment(
+            value: ThemeMode.light,
+            icon: Icon(Icons.light_mode_outlined),
+          ),
+          ButtonSegment(
+            value: ThemeMode.system,
+            icon: Icon(Icons.settings_suggest_outlined),
+          ),
+          ButtonSegment(
+            value: ThemeMode.dark,
+            icon: Icon(Icons.dark_mode_outlined),
+          ),
+        ],
+        onSelectionChanged:
+            ref.read(themeControllerProvider.notifier).onThemeModeChanged,
+      ),
     );
   }
 }
