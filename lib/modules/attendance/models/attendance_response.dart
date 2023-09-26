49fdb074858c057246765dff1fc32defb7ffbbf3
LuckUVeryX <luckuveryx@icloud.com>
2023-09-26 11:11:58 +0800

    feat: attendance API and repo

---
+++ lib/modules/attendance/models/attendance_response.dart
@@ -0,0 +1,29 @@
+import 'package:evlve/modules/facility/facility.dart';
+import 'package:evlve/modules/schedule/schedule.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'attendance_response.freezed.dart';
+part 'attendance_response.g.dart';
+
+@freezed
+class AttendanceResponse with _$AttendanceResponse {
+  const factory AttendanceResponse({
+    required List<Attendance> attendances,
+  }) = _AttendanceResponse;
+
+  factory AttendanceResponse.fromJson(Map<String, dynamic> json) =>
+      _$AttendanceResponseFromJson(json);
+}
+
+@freezed
+class Attendance with _$Attendance {
+  const factory Attendance({
+    required String id,
+    required DateTime date,
+    required String startTime,
+    required Facility facility,
+    required Level level,
+  }) = _Attendance;
+  factory Attendance.fromJson(Map<String, dynamic> json) =>
+      _$AttendanceFromJson(json);
+}
