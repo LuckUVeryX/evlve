99e81992aba45a2f823114df1121cfd45e5d1350
LuckUVeryX <luckuveryx@icloud.com>
2023-09-26 14:26:27 +0800

    feat: pull to refresh attendance

--- lib/modules/attendance/views/attendance_list_view.dart
+++ lib/modules/attendance/views/attendance_list_view.dart
@@ -28,48 +28,56 @@ class _AttendanceListViewState extends ConsumerState<AttendanceListView> {
       ),
     );
 
-    return CustomScrollView(
+    return NestedScrollView(
       controller: _controller,
-      slivers: [
-        SliverAppBar.large(title: Text(context.l10n.attendance)),
-        SliverList.builder(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [SliverAppBar.large(title: Text(context.l10n.attendance))];
+      },
+      body: RefreshIndicator(
+        onRefresh: () async {
+          ref.invalidate(attendanceProvider);
+          return ref.read(
+            attendanceProvider(page: 0, limit: pageSize).future,
+          );
+        },
+        child: ListView.builder(
+          padding: EdgeInsets.zero,
           itemBuilder: (context, index) {
             final page = index ~/ pageSize;
             final indexInPage = index % pageSize;
             final attendanceList = ref.watch(
               attendanceProvider(limit: pageSize, page: page),
             );
+            return attendanceList.unwrapPrevious().when(
+                  loading: () => const AttendanceListItemShimmer(),
+                  error: (e, st) {
+                    if (indexInPage != 0) return null;
+                    return Text('Error $e');
+                  },
+                  data: (attendances) {
+                    if (indexInPage >= attendances.length) return null;
+                    final attendance = attendances[indexInPage];
 
-            return attendanceList.when(
-              loading: () => const AttendanceListItemShimmer(),
-              error: (e, st) {
-                if (indexInPage != 0) return null;
-                return Text('Error $e');
-              },
-              data: (attendances) {
-                if (indexInPage >= attendances.length) return null;
-                final attendance = attendances[indexInPage];
+                    final showDateHeader = indexInPage == 0 ||
+                        attendance.date != attendances[indexInPage - 1].date;
 
-                final showDateHeader = indexInPage == 0 ||
-                    attendance.date != attendances[indexInPage - 1].date;
-
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (showDateHeader)
-                      AttendanceDateHeader(date: attendance.date),
-                    AttendanceListItem(
-                      index: index + 1,
-                      attendance: attendance,
-                    ),
-                  ],
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (showDateHeader)
+                          AttendanceDateHeader(date: attendance.date),
+                        AttendanceListItem(
+                          index: index + 1,
+                          attendance: attendance,
+                        ),
+                      ],
+                    );
+                  },
                 );
-              },
-            );
           },
         ),
-      ],
+      ),
     );
   }
 }
