97e7a27aceb24c1e962dd910a3ce0efb77875a41
LuckUVeryX <luckuveryx@icloud.com>
2023-09-26 18:55:01 +0800

    feat: settings sub pages will cover bottom nav

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -1,19 +1,16 @@
 import 'package:evlve/app/router/router_listenable.dart';
 import 'package:evlve/app/router/routes.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'router.g.dart';
-
-final _key = GlobalKey<NavigatorState>(debugLabel: 'routerKey');
 
 @riverpod
 Raw<GoRouter> router(RouterRef ref) {
   final notifier = ref.watch(routerListenableProvider.notifier);
   final router = GoRouter(
     routes: $appRoutes,
-    navigatorKey: _key,
+    navigatorKey: rootKey,
     debugLogDiagnostics: true,
     redirect: notifier.redirect,
     refreshListenable: notifier,
