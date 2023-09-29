56d1067d09b5cb242308b9c31d4396850fc5250a
LuckUVeryX <luckuveryx@icloud.com>
2023-09-30 00:10:50 +0800

    feat: remove flex color scheme

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,4 +1,3 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,21 +7,15 @@ class AppTheme {
   static const seedColor = Color(0xFFEC1B23);
 
   static ThemeData theme(Brightness brightness, [Color? color]) {
-    final flexColor = FlexSchemeColor.from(primary: color ?? seedColor);
-    final fontFamily = GoogleFonts.dmSans().fontFamily;
+    final colorScheme = ColorScheme.fromSeed(
+      seedColor: color ?? seedColor,
+      primary: color ?? seedColor,
+      brightness: brightness,
+    );
 
-    final base = switch (brightness) {
-      Brightness.light => FlexThemeData.light(
-          colors: flexColor,
-          fontFamily: fontFamily,
-        ),
-      Brightness.dark => FlexThemeData.dark(
-          colors: flexColor,
-          fontFamily: fontFamily,
-        ),
-    };
-
-    final textTheme = base.textTheme.copyWith(
+    final textTheme = GoogleFonts.dmSansTextTheme(
+      ThemeData(brightness: brightness).textTheme,
+    ).copyWith(
       headlineMedium: const TextStyle(fontWeight: FontWeight.bold),
       titleLarge: const TextStyle(fontWeight: FontWeight.bold),
       bodyLarge: const TextStyle(fontWeight: FontWeight.bold),
@@ -32,35 +25,23 @@ class AppTheme {
     final listTileTheme = ListTileThemeData(
       titleTextStyle: textTheme.bodyLarge,
       subtitleTextStyle: textTheme.labelLarge,
-      textColor: base.colorScheme.onBackground,
-    );
-
-    final navigationBarTheme = NavigationBarThemeData(
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-      indicatorColor: base.colorScheme.primary,
-      indicatorShape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(double.infinity),
-      ),
-      iconTheme: MaterialStateProperty.resolveWith((states) {
-        if (!states.contains(MaterialState.selected)) return base.iconTheme;
-        return base.iconTheme.copyWith(
-          color: base.colorScheme.onPrimary,
-        );
-      }),
+      textColor: colorScheme.onBackground,
     );
 
     final inputDecorationTheme = InputDecorationTheme(
       filled: true,
-      fillColor: base.colorScheme.surfaceVariant,
+      fillColor: colorScheme.surfaceVariant,
       errorStyle: textTheme.bodyMedium?.copyWith(
-        color: base.colorScheme.errorContainer,
+        color: colorScheme.errorContainer,
       ),
     );
 
-    return base.copyWith(
+    return ThemeData.from(
+      useMaterial3: true,
+      colorScheme: colorScheme,
       textTheme: textTheme,
+    ).copyWith(
       listTileTheme: listTileTheme,
-      navigationBarTheme: navigationBarTheme,
       inputDecorationTheme: inputDecorationTheme,
     );
   }
