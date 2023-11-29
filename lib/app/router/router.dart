6fadbd11da7e50b46bd0ddad78907a41028b90f8
LuckUVeryX <luckuveryx@icloud.com>
2023-11-29 17:27:23 +0800

    refactor: migrate router provider

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -1,21 +1,53 @@
-import 'package:evlve/app/router/router_listenable.dart';
 import 'package:evlve/app/router/routes.dart';
+import 'package:evlve/modules/auth/auth.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'router.g.dart';
 
 @riverpod
-Raw<GoRouter> router(RouterRef ref) {
-  final notifier = ref.watch(routerListenableProvider.notifier);
+GoRouter router(RouterRef ref) {
+  final authState = ValueNotifier<AsyncValue<AuthState>>(const AsyncLoading());
+  ref
+    ..onDispose(authState.dispose)
+    ..listen(authControllerProvider, (_, value) {
+      authState.value = value;
+    });
+
   final router = GoRouter(
-    routes: $appRoutes,
     navigatorKey: rootKey,
-    debugLogDiagnostics: true,
-    redirect: notifier.redirect,
-    refreshListenable: notifier,
+    refreshListenable: authState,
     initialLocation: SplashRoute.path,
+    debugLogDiagnostics: true,
+    routes: $appRoutes,
+    redirect: (context, state) {
+      if (authState.value.unwrapPrevious().hasError) {
+        return const LoginRoute().location;
+      }
+      if (authState.value.isLoading || !authState.value.hasValue) {
+        return const SplashRoute().location;
+      }
+      final auth = authState.value.requireValue;
+      final isSplash = state.uri.path == const SplashRoute().location;
+      if (isSplash) {
+        return auth.map(
+          loggedIn: (_) {
+            final currentRoute = state.uri.toString();
+            final isLoggingIn =
+                currentRoute.startsWith(const LoginRoute().location);
+            return isLoggingIn ? const ScheduleRoute().location : null;
+          },
+          loggedOut: (_) => const LoginRoute().location,
+          requireOtp: (_) => const OtpRoute().location,
+        );
+      }
+
+      return null;
+    },
   );
+
   ref.onDispose(router.dispose);
+
   return router;
 }
